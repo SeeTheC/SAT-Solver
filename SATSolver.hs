8ff70013c946b65cffae989143294b23c616866a
Anshul Gupta <anshulgupta0803@gmail.com>
2017-11-02 21:23:45 +0530

    Parses clauses and symbols from input file

--- SATSolver.hs
+++ SATSolver.hs
@@ -1,14 +1,38 @@
 import System.Environment
 
--- Returns a list of lists
+type Clause = [Int]
+type Symbol = Int
+
+-- Returns a list of Clauses
 -- Each list is a clause
 parse filename = do
   content <- readFile filename
   let
     conjuctions = lines content
-    parsedConjuctions = map (map read) (map words (lines content)) :: [[Int]]
-  return parsedConjuctions
+    listOfStrings = map words conjuctions
+    listOfClauses = map (map read) listOfStrings :: [Clause]
+  return listOfClauses
+
+mergeClauses [] = []
+mergeClauses (x:xs) = x ++ mergeClauses xs
+
+removeNegatives [] = []
+removeNegatives (x:xs) | x < 0 = (-1 * x) : removeNegatives xs
+                       | otherwise = x : removeNegatives xs
+
+unique [] = []
+unique (x:xs) = x : unique (filter (x /=) xs)
+
+getSymbols clauses = do
+  let
+    symbols = unique $ removeNegatives $ mergeClauses clauses :: [Symbol]
+  return symbols
 
 main = do
   (filename:_) <- getArgs
-  parse filename
+  clauses <- parse filename
+  symbols <- getSymbols clauses
+  putStrLn "Clauses:"
+  print clauses
+  putStrLn "Symbols:"
+  print symbols
